9bb1ff1a3937a36ec1b2bb00eb3d7111c754d28e
Tom Gruintjes <tom.gruintjes@gmail.com>
2018-07-12 21:23:31 +0200

    additional help added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,22 @@ import { Storage } from '@ionic/storage';
 import { Http, Headers, RequestOptions } from "@angular/http";
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-//import { Observable } from "rxjs/Rx"
+
 import * as Clipboard from 'clipboard/dist/clipboard.min.js';
+
+interface entryLine {
+  voornaam: string;
+  tussen: string;
+  achternaam: string;
+  initialen: string;
+  postcode: string;
+  huisnummer: string;
+  email: string;
+  telnr: string;
+  straatnaam: string;
+  geslacht: string;
+  plaats: string;
+}
 
 @Component({
   selector: 'page-home',
@@ -17,7 +31,7 @@ export class HomePage {
 
   apiKey: string = "";
   txtToCopy: string = "";
-  cards: Array<Object> = [];
+  cards: Array<entryLine> = [];
   clipboard: any;
   constructor(public toastCtrl: ToastController, private alertCtrl: AlertController, public navCtrl: NavController, private storage: Storage, private http: Http) {
     this.getStuff();
@@ -48,22 +62,22 @@ export class HomePage {
     this.txtToCopy = "";
     this.cards.map(card => {
 
-      if (card['plaats'] != "") {
+      if (card.plaats != "") {
 
-        if (typeof card['geslacht'] == 'undefined') card['geslacht'] = "";
+        if (typeof card.geslacht == 'undefined') card.geslacht = "";
 
         this.txtToCopy +=
-          csvCell(card['voornaam']) +
-          csvCell(card['initialen']) +
-          csvCell(card['tussen']) +
-          csvCell(card['achternaam']) +
-          csvCell(card['geslacht']) +
-          csvCell(card['straatnaam']) +
-          csvCell(card['huisnummer']) +
-          csvCell(card['postcode']) +
-          csvCell(card['plaats']) +
-          csvCell(card['email']) +
-          quote + card['telnr'] + quote + "\n";
+          csvCell(card.voornaam) +
+          csvCell(card.initialen) +
+          csvCell(card.tussen) +
+          csvCell(card.achternaam) +
+          csvCell(card.geslacht) +
+          csvCell(card.straatnaam) +
+          csvCell(card.huisnummer) +
+          csvCell(card.postcode) +
+          csvCell(card.plaats) +
+          csvCell(card.email) +
+          quote + card.telnr + quote + "\n";
       }
     })
   }
@@ -71,31 +85,30 @@ export class HomePage {
   voornaamChange(voornaam, card) {
     if (voornaam.length > 0) {
       card.initialen = voornaam.charAt(0) + '.';
-      card['geslacht'] = 'm';
-      card['initialen'] = card['initialen'].toUpperCase();
-      // hier toevoegen: geslachtcheck
+      card.geslacht = 'm';
+      card.initialen = card.initialen.toUpperCase();
     }
   }
 
   initialenCheck(card) {
-    card['initialen'] = card['initialen'].toUpperCase();
-    if (card['initialen'].length == 1) card['initialen'] += '.';
+    card.initialen = card.initialen.toUpperCase();
+    if (card.initialen.length == 1) card.initialen += '.';
   }
 
   telNrEntry(card) {
-    if (card['telnr'] == '') card['telnr'] = '06-';
+    if (card.telnr == '') card.telnr = '06-';
   }
 
   telNrCheck(card) {
-    if (card['telnr'] == '06-') card['telnr'] = '';
-   
-    if (card['telnr'].substring(0, 5) == '06-0')
-      card['telnr'] = card['telnr'].substring(3, 1000);
+    if (card.telnr == '06-') card.telnr = '';
 
-    let telnr = card['telnr'];
+    if (card.telnr.substring(0, 4) == '06-0')
+      card.telnr = card.telnr.substring(3, 1000);
+
+    let telnr = card.telnr;
     if (telnr.substring(0, 2) == "06")
       if (telnr.charAt(2) != '-')
-        card['telnr'] = '06' + '-' + telnr.slice(-8)
+        card.telnr = '06' + '-' + telnr.slice(-8)
   }
 
 
@@ -129,39 +142,50 @@ export class HomePage {
   emailComplete(card, event) {
     // console.log('stuff', card);
 
-    let emails = [
+    interface emailMatch {
+      e: string;
+      d: string;
+    }
+
+    let emails: Array<emailMatch> = [
       { e: '@gma', d: '@gmail.com' },
       { e: '@hetn', d: '@hetnet.nl' },
       { e: '@hotm', d: '@hotmail.com' },
       { e: '@quic', d: '@quicknet.nl' },
       { e: '@liv', d: '@live.nl' },
+      { e: '@cai', d: '@caiway.nl' },
       { e: '@kpn', d: '@kpnplanet.nl' },
       { e: '@upc', d: '@upcmail.nl' },
       { e: '@outl', d: '@outook.com' }
     ]
 
     emails.map(entry => {
-      let e = entry['e'];
-      let d = entry['d'];
-      let teste = card['email'].slice(-1 * e.length);
+      let e = entry.e;
+      let d = entry.d;
+      let teste = card.email.slice(-1 * e.length);
       //console.log('check s ',e,d,teste)
 
       if (teste == e) {
         // console.log('HITT')
-        card['email'] = card['email'].replace(e, d);
+        card.email = card.email.replace(e, d);
       }
     })
 
   }
 
+  plaatsEntry(card, i) {
+    if (card.plaats == '' && i > 0)
+      card.plaats = this.cards[i-1].plaats
+  }
+
   postcodeCheck(card) {
 
     let headers = new Headers({ 'X-Api-Key': this.apiKey });
-    let postcode = card['postcode'].replace(/ /g, '').toUpperCase();
-    let number = card['huisnummer']
+    let postcode = card.postcode.replace(/ /g, '').toUpperCase();
+    let number = card.huisnummer
     let url = "https://api.postcodeapi.nu/v2/addresses/?postcode=" + postcode + "&number=" + number;
 
-    if (postcode != "" && card['plaats'] == "")
+    if (postcode != "" && card.plaats == "")
       this.http.get(url, new RequestOptions({ headers: headers }))
         .map(res => res.json())
         .map(res => res['_embedded'])
@@ -169,8 +193,8 @@ export class HomePage {
         .subscribe(data => {
           let city = data['city']['label'];
           let street = data['street']
-          card['straatnaam'] = street;
-          card['plaats'] = city;
+          card.straatnaam = street;
+          card.plaats = city;
           console.log('data ', data)
         }, (err) => {
           this.showMsg('Error search postcode ');
@@ -178,7 +202,7 @@ export class HomePage {
         })
 
     // and clean postcode
-    card['postcode'] = postcode; //.substring(0, 4) + postcode.slice(-2);
+    card.postcode = postcode; //.substring(0, 4) + postcode.slice(-2);
   }
 
 
